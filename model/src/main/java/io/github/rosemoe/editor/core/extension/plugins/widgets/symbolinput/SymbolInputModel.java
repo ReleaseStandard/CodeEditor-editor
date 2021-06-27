c7710c25b4f094031aad729a486951ef74f9c430
Release Standard <releasestandard@netc.eu>
2021-06-27 21:38:26 +0200

    Extract symbol input from the tree

--- model/src/main/java/io/github/rosemoe/editor/core/extension/plugins/widgets/symbolinput/SymbolInputModel.java
+++
@@ -1,6 +0,0 @@
-package io.github.rosemoe.editor.core.extension.plugins.widgets.symbolinput;
-
-import java.util.HashMap;
-
-public class SymbolInputModel {
-}
--- model/src/main/java/io/github/rosemoe/editor/core/extension/plugins/widgets/symbolinput/SymbolModel.java
+++
@@ -1,6 +0,0 @@
-package io.github.rosemoe.editor.core.extension.plugins.widgets.symbolinput;
-
-public class SymbolModel {
-    protected String displayIcon = "";
-    protected String actionText = "";
-}
