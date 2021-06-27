431862e21d0356e28469c00f4fd6acbda44d01d2
Release Standard <releasestandard@netc.eu>
2021-06-27 15:53:48 +0200

    Remove line number widget from the tree

--- view-android/src/main/java/io/github/rosemoe/editor/core/extension/plugins/widgets/linenumberpanel/handles/LineNumberPanelViewHandles.java
+++
@@ -1,9 +0,0 @@
-package io.github.rosemoe.editor.core.extension.plugins.widgets.linenumberpanel.handles;
-
-import android.graphics.Canvas;
-
-public class LineNumberPanelViewHandles {
-    public void handleOnDraw(Object canvas) {
-
-    }
-}
