7ad39c17e5fb69639a0482b47ab33656f7e0eda1
Release Standard <releasestandard@netc.eu>
2021-08-17 09:49:53 +0000

    Move parts to the model

---
+++ model/src/test/java/io/github/rosemoe/editor/core/langs/helpers/TrieTreeTest.java
@@ -0,0 +1,20 @@
+package io.github.rosemoe.editor.core.langs.helpers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrieTreeTest {
+
+    @Test
+    public void put() {
+    }
+
+    @Test
+    public void testPut() {
+    }
+
+    @Test
+    public void get() {
+    }
+}
