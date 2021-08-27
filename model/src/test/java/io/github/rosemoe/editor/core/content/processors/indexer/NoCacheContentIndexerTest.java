d237e2b30dbfa81f31ae9d9c2dc5ffbd8ff0c8fd
Release Standard <releasestandard@netc.eu>
2021-08-27 18:16:13 +0200

    Not well defined NoCacheContentIndexer getCharPosition

--- model/src/test/java/io/github/rosemoe/editor/core/content/processors/indexer/NoCacheContentIndexerTest.java
+++ model/src/test/java/io/github/rosemoe/editor/core/content/processors/indexer/NoCacheContentIndexerTest.java
@@ -1,5 +1,6 @@
 package io.github.rosemoe.editor.core.content.processors.indexer;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import io.github.rosemoe.editor.core.content.CodeAnalyzerResultContent;
@@ -33,11 +34,27 @@ public class NoCacheContentIndexerTest {
             assertTrue(indexer.getCharPosition(new CharPosition(1)) != null);
             assertTrue(indexer.getCharPosition(new CharPosition(2)) != null);
             assertTrue(indexer.getCharPosition(new CharPosition(3)) != null);
-            assertTrue(indexer.getCharPosition(new CharPosition(4)) == null);
+            assertTrue(indexer.getCharPosition(new CharPosition(4)) != null);
+            assertTrue(indexer.getCharPosition(new CharPosition(5)) == null);
             assertTrue(indexer.getCharPosition(new CharPosition(0,0)) != null);
             assertTrue(indexer.getCharPosition(new CharPosition(0,1)) != null);
             assertTrue(indexer.getCharPosition(new CharPosition(0,2)) != null);
+            assertTrue(indexer.getCharPosition(new CharPosition(0,3)) != null);
             assertTrue(indexer.getCharPosition(new CharPosition(1,0)) != null);
+            assertTrue(indexer.getCharPosition(new CharPosition(1,1)) != null);
+            assertTrue(indexer.getCharPosition(new CharPosition(2,0)) != null);
+
         }
+    }
+    @Test
+    @Ignore("This is know")
+    public void testCharPosition() {
+        // a
+        CodeAnalyzerResultContent content = new CodeAnalyzerResultContent();
+        content.append("a");
+        @Jailbreak NoCacheContentIndexer indexer = new NoCacheContentIndexer(content);
+        assertTrue(indexer.getCharPosition(new CharPosition(0,0)) != null);
+        assertTrue(indexer.getCharPosition(new CharPosition(0,1)) != null);
+        assertTrue(indexer.getCharPosition(new CharPosition(0,2)) == null);
     }
 }
